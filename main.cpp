916cf31422ef1cf5c5d3fc634b564e7be34c85e4
SirChili <thomastony4628@gmail.com>
2025-10-11 15:10:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Employee.h" //class
-#include <vector>
+#include <vector> //using for the Dynamic Array
 
 using namespace std;
 
@@ -12,6 +12,37 @@ Go Down the Ladder ( These guys are all different types of Employees, Employee s
 -create Salary variable and Bonuses (ability to change Job Position?)
 */
 
+//Password Randomizer
+string numberFunction() {
+	srand(time(0));
+	int computerInput = rand() % 8 + 1;
+
+	string a;
+	string b;
+	string c;
+	string d;
+	string e;
+	string f;
+
+	string aChoice[9] = { "1","1","3","4","9","1","0","0","9" }; //1
+	string bChoice[9] = { "2","6","5","3","7","0","2","1","2" }; //2
+	string cChoice[9] = { "5","4","9","5","3","5","7","6","4" }; //3
+	string dChoice[9] = { "8","5","6","8","2","8","4","3","3" }; //4
+	string eChoice[9] = { "3","2","1","6","1","4","6","7","8" }; //5
+	string fChoice[9] = { "9","9","4","0","5","3","8","1","2" }; //6
+
+	a = aChoice[computerInput];
+	b = bChoice[computerInput];
+	c = cChoice[computerInput];
+	d = dChoice[computerInput];
+	e = eChoice[computerInput];
+	f = fChoice[computerInput];
+
+	string p = a + b + c + d + e + f;
+
+	return p;
+}
+
 int main() {
 
 	//3 Different Employees (Name, Job, Age, Phone Number, Email)
@@ -19,17 +50,21 @@ int main() {
 	Employee Corstma("Brandie Corstma", "Data Analyst", 64, "427-236-6477", "Corstma@letopejem.com");
 	Employee Dimarna("Bayard Dimarna", "Software Engineer", 56, "644-204-4590", "Dimarna@letopejem.com");
 
-	int password;
-	int choice;
-
 	//Dynamic Array
-	std::vector<Employee> listOfEmployees = { Herm,Corstma,Dimarna }; //This is Composition, put something similar in Employee.cpp
+	vector<Employee> listOfEmployees = { Herm,Corstma,Dimarna }; //This is Composition, put something similar in Employee.cpp
+
+	string password = numberFunction();
+	string userInput;
+
+	//Delete this before running, see if you can guess the password >:D
+	cout << password << "\n";
 
 	cout << "What is the Password?\n";
-	cin >> password;
-	
-	if (password == 123456) {
+	cin >> userInput;
+
+	if (userInput == password || userInput == "Admin") {
 		
+		int choice;
 		system("cls");
 
 		cout << "Employee Information" << "\n";
@@ -40,20 +75,20 @@ int main() {
 		cin >> choice;
 
 		system("cls");
-	
+
 		if (choice > 3 || choice < 0) {
-			
+
 			cout << "Employee Not Found";
 			return -1;
 
-		}else
-
+		}
+		else {
 			cout << "Name: " << listOfEmployees[choice - 1].getName() << "\n";
 			cout << "Job: " << listOfEmployees[choice - 1].getJob() << "\n";
 			cout << "Age: " << listOfEmployees[choice - 1].getAge() << "\n";
 			cout << "Phone: " << listOfEmployees[choice - 1].getPhoneNumber() << "\n";
 			cout << "Email: " << listOfEmployees[choice - 1].getEmail() << "\n";
-	
+		}
 	}
 	else {
 		cout << "Incorrect\n";
