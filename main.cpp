e3ca3ff818912a256db2a2ad1a5e95e8d180ad72
SirChili <thomastony4628@gmail.com>
2025-10-11 10:51:27 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "Employee.h" //class
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	//3 Different Employees (Name, Job, Age, Phone Number, Email)
+	Employee Herm("Sherwynd Herm", "Data Analyst", 31, "153-760-6912", "Herm@letopejem.info");
+	Employee Corstma("Brandie Corstma", "Data Analyst", 64, "427-236-6477", "Corstma@letopejem.com");
+	Employee Dimarna("Bayard Dimarna", "Software Engineer", 56, "644-204-4590", "Dimarna@letopejem.com");
+
+	int password;
+	int choice;
+
+	//Dynamic Array
+	std::vector<Employee> listOfEmployees = { Herm,Corstma,Dimarna };
+
+	cout << "What is the Password?\n";
+	cin >> password;
+	
+	if (password == 123456) {
+		
+		system("cls");
+
+		cout << "Employee Information" << "\n";
+		cout << "1. " << listOfEmployees[0].getName() << "\n";
+		cout << "2. " << listOfEmployees[1].getName() << "\n";
+		cout << "3. " << listOfEmployees[2].getName() << "\n";
+
+		cin >> choice;
+
+		system("cls");
+	
+		if (choice > 3 || choice < 0) {
+			
+			cout << "Employee Not Found";
+			return -1;
+
+		}else
+
+			cout << "Name: " << listOfEmployees[choice - 1].getName() << "\n";
+			cout << "Job: " << listOfEmployees[choice - 1].getJob() << "\n";
+			cout << "Age: " << listOfEmployees[choice - 1].getAge() << "\n";
+			cout << "Phone: " << listOfEmployees[choice - 1].getPhoneNumber() << "\n";
+			cout << "Email: " << listOfEmployees[choice - 1].getEmail() << "\n";
+	
+	}
+	else {
+		cout << "Incorrect\n";
+	}
+}
