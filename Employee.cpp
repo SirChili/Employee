9cdeee9897fb9ce23c963bfc182c97c767138129
SirChili <thomastony4628@gmail.com>
2025-10-11 10:52:14 -0500

    Create Employee.cpp

---
+++ Employee.cpp
@@ -0,0 +1,63 @@
+#include "Employee.h"
+
+void Employee::setName(string n)
+{
+	name = n;
+}
+
+string Employee::getName()
+{
+	return name;
+}
+
+void Employee::setJob(string j)
+{
+	job = j;
+}
+
+string Employee::getJob()
+{
+	return job;
+}
+
+void Employee::setAge(int a)
+{
+	age = a;
+}
+
+int Employee::getAge()
+{
+	return age;
+}
+
+void Employee::setPhoneNumber(string p)
+{
+	phone = p;
+}
+
+string Employee::getPhoneNumber()
+{
+	return phone;
+}
+
+void Employee::setEmail(string e)
+{
+	email = e;
+}
+
+string Employee::getEmail()
+{
+	return email;
+}
+
+//Takes away the warning
+Employee::Employee() = default;
+
+Employee::Employee(string n, string j, int a, string p, string e)
+{
+	name = n;
+	job = j;
+	age = a;
+	phone = p;
+	email =e;
+}
