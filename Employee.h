0ce72c7116f43330afa3563d39d57dea30a3846f
SirChili <thomastony4628@gmail.com>
2025-10-11 10:51:50 -0500

    Create Employee.h

---
+++ Employee.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Employee
+{
+private:
+	string name;
+	string job;
+	int age{}; //Curly braces Initializes age to 0
+	string phone{}; //Curly braces Initializes phone to 0
+	string email;
+
+public:
+	
+	void setName(string n);
+	string getName();
+
+	void setJob(string j);
+	string getJob();
+	
+	void setAge(int a);
+	int getAge();
+	
+	void setPhoneNumber(string p);
+	string getPhoneNumber();
+
+	void setEmail(string e);
+	string getEmail();
+
+	Employee();
+	Employee(string n,string j,int a,string p,string e);
+};
