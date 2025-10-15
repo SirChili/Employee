3229f673bc4ee7cb732b55bb7f25c58ff610f52e
SirChili <thomastony4628@gmail.com>
2025-10-15 14:01:26 -0500

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -5,15 +5,15 @@ using namespace std;
 
 class Employee
 {
-private:
+//private:
+protected: //protected means CHILDREN classes of this class have access to these
 	string name;
 	string job;
 	int age{}; //Curly braces Initializes age to 0
 	string phone{}; //Curly braces Initializes phone to 0
 	string email;
-
+	double salary;
 public:
-
 	void setName(string n);
 	string getName();
 
@@ -29,17 +29,42 @@ public:
 	void setEmail(string e);
 	string getEmail();
 
+	void setSalary(double s);
+
+	double getSalary();
+
 	Employee();
-	Employee(string n, string j, int a, string p, string e);
+	Employee(string n, string j, int a, string p, string e,double s);
 };
 
 
-class Manager : public Employee 
+class Manager : public Employee
 {
+private:
+	string manPass;
+	//Employee stuff; //not a good idea
+public:
 
+	void setManPass(string pass);
+	string getManPass();
+
+	Manager();
+	Manager(string n, string j, int a, string p, string e, double s, string pass);
 };
 
-class Executive : public Employee 
+
+
+
+class Executive : public Employee
 {
-	
+private:
+	string execPass;
+public:
+
+	void setExecPass(string pass);
+
+	string getExecPass();
+
+	Executive();
+	Executive(string n, string j, int a, string p, string e,double s, string pass);
 };
